373dbebba602e79eb51425098f8c07649621caf7
Abu Naim <abunaim@Abus-Air-2.lan>
2024-01-15 03:27:20 +0600

    DTL

--- first_app/views.py
+++ first_app/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
+import datetime
 
 # when we give data back end to fornt end with a dictionary it is called context
 
 def home(request):
-    d = {'author' : 'Naim', 'age' : 10, 'lst' : [1,2,3,4], 'courses' : [
+    d = {'author' : 'Naim', 'age' : 10, 'joining' : ['python', 'is', 'best', 'programming', 'language'], 'birthday' : datetime.datetime.now(), 'lst' : [1,2,3,4], 'emp' : '','courses' : [
             {
                 'id' : 1,
                 'name' : 'python',
