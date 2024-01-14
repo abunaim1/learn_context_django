1c2a7c0f29f729f2182a1a69db1009a5e629ecd5
Abu Naim <abunaim@Abus-Air-2.lan>
2024-01-14 21:49:26 +0600

    context

--- first_app/views.py
+++ first_app/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+# when we give data back end to fornt end with a dictionary it is called context
+
+def home(request):
+    d = {'author' : 'Naim', 'age' : 20, 'lst' : [1,2,3,4]}
+    return render(request,'home.html', d)
