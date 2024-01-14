a5fcbd76b39b12eea58d3161912dcf00e00d1a48
Abu Naim <abunaim@Abus-Air-2.lan>
2024-01-15 00:56:31 +0600

    template filtering

--- first_app/views.py
+++ first_app/views.py
@@ -3,5 +3,22 @@ from django.shortcuts import render
 # when we give data back end to fornt end with a dictionary it is called context
 
 def home(request):
-    d = {'author' : 'Naim', 'age' : 20, 'lst' : [1,2,3,4]}
+    d = {'author' : 'Naim', 'age' : 10, 'lst' : [1,2,3,4], 'courses' : [
+            {
+                'id' : 1,
+                'name' : 'python',
+                'fee' : 10000
+            },
+            {
+                'id' : 2,
+                'name' : 'Django',
+                'fee' : 12000
+            },
+            {
+                'id' : 3,
+                'name' : 'C',
+                'fee' : 2000
+            },
+        ]
+    }
     return render(request,'home.html', d)
